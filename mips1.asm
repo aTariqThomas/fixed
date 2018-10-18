3eef8b820906b913e06e1cb980ae1a9233fc9741
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-10-18 16:44:01 -0400

    Corrected the code
    I basically started over with my corrected code.

--- mips1.asm
+++ mips1.asm
@@ -1,17 +1,10 @@
-.data   #Field that takes in data
- myCharacter1:  .byte 'T' #Takes the given character
- myCharacter2: .byte 'A' #Takes the given character
- myCharacter3: .byte 'R' #Takes the given character
- myCharacter4: .byte 'I' #Takes the given character
- myCharacter5: .byte 'Q' #Takes the given character
- 
- myID : #Preps the space for my ID 
- .text  #Takes in text
- li $v0, 4 #Gets the machine ready for code
- la $a0, myCharacter1 #Places the first letter in the register
- syscall #Executes the system call
- la $a1, myCharacter2 #Places the second letter in the register
- la $a2 myCharacter3 #Placed the third letter in the register
- la $a3 myCharacter4 #Placed the fourth letter in the register
- syscall #Executes the system call
+.text 
+main:
+li $v0, 11  # Loads character
+la $a0, 84  #Puts T in the register
+syscall #Executes the call
+addi $a0,$a0,-19 #Puts A in the Register 
+syscall 
+li $v0, 10 #Exit Call
+syscall 
  
