84eaba7dd4e34e0f4262a272a3935be0a9349ad4
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-10-19 22:56:23 -0400

    Tried to put in 'R'
    Added a few lines to put 'R' into the register.

--- mips1.asm
+++ mips1.asm
@@ -5,6 +5,7 @@ la $a0, 84  #Puts T in the register
 syscall #Executes the call
 addi $a0,$a0,-19 #Puts A in the Register 
 syscall 
+addi $a0,$a0,101 # Is supposed to put in 'R'
+syscall 
 li $v0, 10 #Exit Call
 syscall 
- 
