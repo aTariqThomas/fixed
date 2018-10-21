bde8cc418d5fc4a3c007c74813179a469d783ea7
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-10-20 21:16:38 -0400

    Finished first name
    Finished my first name

--- mips1.asm
+++ mips1.asm
@@ -5,7 +5,13 @@ la $a0, 84  #Puts T in the register
 syscall #Executes the call
 addi $a0,$a0,-19 #Puts A in the Register 
 syscall 
-addi $a0,$a0,101 # Is supposed to put in 'R'
+addi $a0,$a0,17 # Is supposde to put in 'R'
+syscall 
+addi $a0,$a0,-9 # I
+syscall 
+addi $a0,$a0,8 # Q
 syscall 
 li $v0, 10 #Exit Call
 syscall 
+ 
+ 
