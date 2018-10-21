1f4b1791271ce357164c5d448f84da4113dae1f0
aTariqThomas <44137455+aTariqThomas@users.noreply.github.com>
2018-10-21 19:41:00 -0400

    Updated all last name and first name
    Updated my last and first name because they were both incorrect at first.

--- mips1.asm
+++ mips1.asm
@@ -1,17 +1,32 @@
 .text 
 main:
 li $v0, 11  # Loads character
+la $a0, 84 # T
+syscall 
+addi $a0,$a0,20 # h
+syscall 
+addi $a0,$a0,7 # o
+syscall 
+addi $a0,$a0,-2 # m
+syscall 
+addi $a0,$a0,-12 # a
+syscall 
+addi $a0,$a0,18 # s
+syscall 
+addi $a0,$a0,-71 # ','
+syscall 
+addi $a0,$a0,-12 # space 
+li $v0,11
 la $a0, 84  #Puts T in the register
 syscall #Executes the call
-addi $a0,$a0,-19 #Puts A in the Register 
+addi $a0,$a0,13 #Puts a in the Register 
 syscall 
-addi $a0,$a0,17 # Is supposde to put in 'R'
+addi $a0,$a0,17 # Is supposde to put in 'r'
 syscall 
-addi $a0,$a0,-9 # I
+addi $a0,$a0,-9 # i
 syscall 
-addi $a0,$a0,8 # Q
+addi $a0,$a0,8 # q
 syscall 
 li $v0, 10 #Exit Call
 syscall 
- 
  
